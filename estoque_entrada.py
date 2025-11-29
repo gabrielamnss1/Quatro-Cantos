a89697fd8460a266525f5b5ff8029b69de9f0e10
joelc-lab <joel.c@ba.estudante.senai.br>
2025-11-29 20:51:30 -0300

    Criando função cadastrar produto

---
+++ estoque_entrada.py
@@ -0,0 +1,22 @@
+from src.database import Produto
+
+def cadastrar_produto(db_session):
+   
+    
+    print("\n" + "="*50)
+    print("   MÓDULO 2: ENTRADA DE ESTOQUE")
+    print("="*50)
+    
+    # ========================================================================
+    # PASSO 1: DEFINIR QUANTOS PRODUTOS SERÃO CADASTRADOS
+    # ========================================================================
+    try:
+        qtd_cadastro = int(input("\n Quantos produtos deseja cadastrar agora? "))
+        
+        if qtd_cadastro <= 0:
+            print("\n Quantidade deve ser maior que zero!")
+            return
+            
+    except ValueError:
+        print("\n Erro: Digite apenas números inteiros!")
+        return
